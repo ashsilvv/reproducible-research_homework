34a768ba2dc7b7136fb973f7c67d212c0bc45cbe
Ash Silva <ash.silva@lmh.ox.ac.uk>
2023-11-30 16:35:45 +0000

    For assignment question 4

--- assignment-brownian-motion.R
+++ assignment-brownian-motion.R
@@ -1,10 +1,5 @@
-
 #install.packages("ggplot2")
 #install.packages("gridExtra")
-
-sink(file = "assignment-package-versions.txt")
-sessionInfo()
-sink()
 
 library(ggplot2)
 library(gridExtra)
@@ -12,16 +7,27 @@ library(gridExtra)
 set.seed(123)
 
 random_walk  <- function (n_steps) {
+  
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
+  
   df[1,] <- c(0,0,1)
+  
   for (i in 2:n_steps) {
+    
     h <- 0.25
+    
     angle <- runif(1, min = 0, max = 2*pi)
+    
     df[i,1] <- df[i-1,1] + cos(angle)*h
+    
     df[i,2] <- df[i-1,2] + sin(angle)*h
+    
     df[i,3] <- i
+    
   }
+  
   return(df)
+  
 }
 
 set.seed(123)
@@ -29,22 +35,27 @@ set.seed(123)
 data1 <- random_walk(500)
 
 plot1 <- ggplot(aes(x = x, y = y), data = data1) +
+  
   geom_path(aes(colour = time)) +
+  
   theme_bw() +
+  
   xlab("x-coordinate") +
+  
   ylab("y-coordinate")
 
-
 set.seed(456)
+
 data2 <- random_walk(500)
 
 plot2 <- ggplot(aes(x = x, y = y), data = data2) +
+  
   geom_path(aes(colour = time)) +
+  
   theme_bw() +
+  
   xlab("x-coordinate") +
+  
   ylab("y-coordinate")
 
-
-
 grid.arrange(plot1, plot2, ncol=2)
-
